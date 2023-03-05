346c060860fc4a5814d71cda2bea884e6669943e
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-05 15:45:45 -0300

    build: main files

---
+++ lib/OneBit_Ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "OneBit_Ruby/version"
+
+module OneBitRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/OneBit_Ruby_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.describe OneBitRuby do
+  it "has a version number" do
+    expect(OneBitRuby::VERSION).not_to be nil
+  end
+
+  end

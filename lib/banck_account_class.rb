c2d69740f3008a56a0ff35fca5286a35bbf28206
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-06 18:38:49 -0300

    refactor: bank account

--- lib/banck_account_class.rb
+++ lib/banck_account_class.rb
@@ -1,12 +1,21 @@
+# frozen_string_literal: true
+
 class BankAccount
   attr_reader :amount
+
   def initialize(amount)
     @amount = amount.to_f.round(2)
   end
+
   def deposit(deposit)
-    (deposit + amount).to_f
+    puts "Deposit of $#{deposit} dollars successful.\nYour new total amount is: $#{(deposit + amount).to_f} dollars."
   end
+
   def withdraw(withdraw)
-    (amount - withdraw).to_f
+    if amount < withdraw
+      puts "Not enough founds."
+    else
+      puts "Withdraw of $#{withdraw} successful.\nYour new total amount is: $#{(amount - withdraw).to_f} dollars."
+    end
   end
 end

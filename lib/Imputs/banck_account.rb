cfceadecb034207501642bad3575e2d44ec8c912
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-06 16:40:12 -0300

    feat: bank account withdraw

--- lib/Imputs/banck_account.rb
+++ lib/Imputs/banck_account.rb
@@ -1,4 +1,8 @@
 require "banck_account_class"
 
-bankaccoun = BankAccount.new(1000)
-puts "You have a total of $#{bankaccoun.amount} dollars"
+bankaccoun = BankAccount.new(1000.00)
+puts "\nYou have a total of $#{bankaccoun.amount} dollars"
+
+puts "How much do you want withdraw $:"
+withdraw = gets.chomp.to_f
+puts "\nYou have a total of $#{bankaccoun.withdraw(withdraw)} dollars"
